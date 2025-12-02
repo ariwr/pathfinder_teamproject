769fae8340f0a2525c4e7f6316f80a7a8eb59b81
yujin lee <betty6004@gmail.com>
2025-12-03 00:28:10 +0900

    a파트 테스트 완료

---
+++ actions.c
@@ -0,0 +1,14 @@
+#include "cleanup.h"
+
+// 나중에 B파트가 구현할 함수들 (지금은 비워둠)
+void move_file(FileInfo *file, const char *dest_folder) {
+    // 텅 빔
+}
+
+void archive_files(FileInfo *head, const char *archive_name) {
+    // 텅 빔
+}
+
+int check_duplicate(FileInfo *file1, FileInfo *file2) {
+    return 0;
+}
---
+++ utils.c
@@ -0,0 +1,10 @@
+#include "cleanup.h"
+
+// B파트가 구현할 함수들
+void write_log(const char *message) {
+    // 텅 빔
+}
+
+void load_config(const char *config_file) {
+    // 텅 빔
+}
